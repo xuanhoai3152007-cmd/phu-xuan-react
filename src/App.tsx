94165bf4adb93c907e6c37d1fdaf14b5603dcb18
ASUS <xuanhoai3152007@gmail.com>
2026-09-20 20:20:50 +0700

    feat: hoàn thành buổi 5 lab 3 bộ lọc danh mục

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
+import { useState } from 'react'
 import AttractionList from './components/AttractionList'
+import CategoryTabs from './components/CategoryTabs'
 import { attractions } from './data/attractions'
 
 function App() {
+  const [selectedCategory, setSelectedCategory] =
+    useState('Tất cả')
+
+  const categories = [
+    'Tất cả',
+    ...new Set(
+      attractions.map((item) => item.category),
+    ),
+  ]
+
+  const filteredAttractions =
+    selectedCategory === 'Tất cả'
+      ? attractions
+      : attractions.filter(
+          (item) =>
+            item.category === selectedCategory,
+        )
+
   return (
     <div className="app">
       <h1>Khám phá Huế</h1>
 
-      <AttractionList attractions={attractions} />
+      <CategoryTabs
+        categories={categories}
+        selectedCategory={selectedCategory}
+        onSelectCategory={setSelectedCategory}
+      />
+
+      <AttractionList
+        attractions={filteredAttractions}
+      />
     </div>
   )
 }
