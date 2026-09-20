f05a2bb46255d9fdeef8c8c0e98c6d8057d850c3
ASUS <xuanhoai3152007@gmail.com>
2026-09-20 18:36:44 +0700

    docs: hoàn thành lab 3 đọc hiểu cấu trúc React

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>phu-xuan-react</h1>
+      <p>Dự án xuyên suốt học phần Web FrontEnd nâng cao</p>
+      <p>Trường Đại học Phú Xuân — Khoa Công nghệ thông tin</p>
+      <p>Sinh viên: Tên của bạn</p>;
+    </div>
+  )
+}
+
+export default App
